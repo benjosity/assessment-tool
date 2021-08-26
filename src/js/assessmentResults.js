fa1ebc8aff9bfdfb4a30af2ae10286e6c60ffa88
Benji Huggins <benji.huggins@thecroc.com>
2021-08-26 10:14:10 +0100

    Start of Assessment Results Calculation

---
+++ src/js/assessmentResults.js
@@ -0,0 +1,9 @@
+const showResults = () => {
+  console.log('show results');
+};
+const calculateResults = () => {
+  console.log('calculate');
+  showResults();
+};
+
+export default calculateResults;
--- src/js/navValidation.js
+++ src/js/navValidation.js
@@ -1,8 +1,8 @@
+import { gsap } from 'gsap';
 const navValidation = (activeQuestion, nextQuestion) => {
   let selectedInputs = nextQuestion.querySelectorAll('.input-element.selected');
   let prevButton = document.getElementById('prevButton');
   let nextButton = document.getElementById('nextButton');
-  let nextQuestionClasses = nextQuestion.classList;
 
   activeQuestion.classList.remove('active');
   nextQuestion.classList.add('active');
@@ -10,6 +10,13 @@ const navValidation = (activeQuestion, nextQuestion) => {
   if (!nextQuestion.classList.contains('question-1')) {
     prevButton.classList.add('enabled');
     prevButton.classList.remove('disabled');
+  }
+
+  if (nextQuestion.classList.contains('question-7')) {
+    let tl = gsap.timeline({ defaults: { duration: 0.3, ease: 'power2.out' } }); //create the timeline
+    tl.to('#nextButton', { x: '-100%', opacity: 0, delay: 0.6 }) //start sequencing
+      .to('#resultsButton', { x: 0, opacity: 0.5 });
+  } else {
   }
 
   if (typeof selectedInputs !== 'undefined' && selectedInputs.length === 0) {
